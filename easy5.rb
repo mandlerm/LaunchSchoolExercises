f686b50eaae38bf8222fb3224ef6caa8cb3ef7a3
mandlerm <amaya.melissa@gmail.com>
2019-01-05 16:14:10 -0600

    working solution for swap

--- easy5.rb
+++ easy5.rb
@@ -81,25 +81,47 @@
 #   s_num
 # end   
 
-MINUTES_PER_HOUR = 60
-HOURS_PER_DAY = 24
-MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
+# MINUTES_PER_HOUR = 60
+# HOURS_PER_DAY = 24
+# MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
 
-def time_of_day(delta_minutes)
-  delta_minutes =  delta_minutes % MINUTES_PER_DAY
-  p delta_minutes
-  hours, minutes = delta_minutes.divmod(MINUTES_PER_HOUR)
+# def time_of_day(delta_minutes)
+#   delta_minutes =  delta_minutes % MINUTES_PER_DAY
+#   p delta_minutes
+#   hours, minutes = delta_minutes.divmod(MINUTES_PER_HOUR)
 
-  format('%02d:%02d', hours, minutes)
-end
+#   format('%02d:%02d', hours, minutes)
+# end
 
 
 
-p time_of_day(0) == "00:00"
+# p time_of_day(0) == "00:00"
 
-p time_of_day(35) == "00:35"
-p time_of_day(3000) == "02:00"
-p time_of_day(800) == "13:20"
-p time_of_day(-4231) == "01:29"
-p time_of_day(-3) == "23:57"
-p time_of_day(-1437) == "00:03"
+# p time_of_day(35) == "00:35"
+# p time_of_day(3000) == "02:00"
+# p time_of_day(800) == "13:20"
+# p time_of_day(-4231) == "01:29"
+# p time_of_day(-3) == "23:57"
+# p time_of_day(-1437) == "00:03"
+
+
+
+####. LETTER SWAP
+
+def swap(string)
+
+  swapped = string.split.map do |word|
+    first = word[0]
+    word[0] = word[-1]
+    word[-1] = first
+    word
+  end 
+
+swapped.join(' ')
+
+
+ end  
+
+ p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
+p swap('Abcde') == 'ebcdA'
+p swap('a') == 'a'
