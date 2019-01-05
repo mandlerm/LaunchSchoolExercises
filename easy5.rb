1f36590c0a3950f958bee2b1d4684f35b72e8509
mandlerm <amaya.melissa@gmail.com>
2019-01-05 17:08:41 -0600

    working solution for part 2

--- easy5.rb
+++ easy5.rb
@@ -153,22 +153,33 @@
 #########. Letter Counter Pt 1
 
 
+# def word_sizes(sentence)
+#   hsh = Hash.new(0)
+    
+#   sentence.split.each do |word|
+#     hsh[word.size] += 1
+#   end 
+#   hsh
+# end 
+
+####. PART 2
+
 def word_sizes(sentence)
+  hsh = Hash.new(0)
   
-  sentence.split.each_with_object({}) do |word, hsh|
-    size = word.size 
-
-     if hsh[size] != nil
-      hsh[size] += 1
-    else 
-      hsh[size] = 1
-    end 
-
+  sentence.split.each do |word|
+    word.gsub!(/[^a-zA-Z]/, '')
+    hsh[word.size] += 1
   end 
-
+  hsh
 end 
 
-
-p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
-p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
 p word_sizes('') == {}
+
+
+# p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+# p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+# p word_sizes('') == {}
