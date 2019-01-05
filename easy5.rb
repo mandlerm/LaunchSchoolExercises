d2383777ee671322875388427c0a73b03ac20b02
mandlerm <amaya.melissa@gmail.com>
2019-01-05 16:06:35 -0600

    start of letter swap

--- easy5.rb
+++ easy5.rb
@@ -26,60 +26,74 @@
 #
 
 
-def time_of_day(time)
-  starting_hours = 24
+# def time_of_day(time)
+#   starting_hours = 24
 
-  hours = time / 60
-  minutes = time % 60
-  s_hours = ''
-  s_mins = ''
-  military_time = ''
-  positive = hours > 0 ? true : false
+#   hours = time / 60
+#   minutes = time % 60
+#   s_hours = ''
+#   s_mins = ''
+#   military_time = ''
+#   positive = hours > 0 ? true : false
 
-  if hours == 0 && minutes == 0
-    s_hours = "00"
-    s_mins = "00"        
-  else 
+#   if hours == 0 && minutes == 0
+#     s_hours = "00"
+#     s_mins = "00"        
+#   else 
     
-    s_mins = num_string(minutes)
+#     s_mins = num_string(minutes)
    
-    if hours == 0 || hours.abs == 24
-      s_hours = '00'
-    elsif (1..23).include?(hours)
-       s_hours = num_string(hours)
-    elsif hours > 24
-      while hours > 24 
-        hours -= 24
-      end
+#     if hours == 0 || hours.abs == 24
+#       s_hours = '00'
+#     elsif (1..23).include?(hours)
+#        s_hours = num_string(hours)
+#     elsif hours > 24
+#       while hours > 24 
+#         hours -= 24
+#       end
       
-      s_hours = num_string(hours)
-    else 
+#       s_hours = num_string(hours)
+#     else 
       
-      while hours < 0 
-        hours += 24
-      end
-        s_hours = num_string(hours)
-    end  
+#       while hours < 0 
+#         hours += 24
+#       end
+#         s_hours = num_string(hours)
+#     end  
     
-  end
+#   end
 
-  # p s_hours, "++++++++++++"
+#   # p s_hours, "++++++++++++"
 
-  military_time = "#{s_hours}:#{s_mins}"
+#   military_time = "#{s_hours}:#{s_mins}"
   
-  military_time
-end 
+#   military_time
+# end 
 
-def num_string(num)
-  s_num = ''
+# def num_string(num)
+#   s_num = ''
   
-  if num.to_s.size < 2 
-    s_num = num.to_s.prepend('0') 
-  else 
-    s_num = num.to_s
-  end
-  s_num
-end   
+#   if num.to_s.size < 2 
+#     s_num = num.to_s.prepend('0') 
+#   else 
+#     s_num = num.to_s
+#   end
+#   s_num
+# end   
+
+MINUTES_PER_HOUR = 60
+HOURS_PER_DAY = 24
+MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
+
+def time_of_day(delta_minutes)
+  delta_minutes =  delta_minutes % MINUTES_PER_DAY
+  p delta_minutes
+  hours, minutes = delta_minutes.divmod(MINUTES_PER_HOUR)
+
+  format('%02d:%02d', hours, minutes)
+end
+
+
 
 p time_of_day(0) == "00:00"
 
