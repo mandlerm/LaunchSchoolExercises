946dcb2a1123ce2614235e8b39fec6d69f682f04
mandlerm <amaya.melissa@gmail.com>
2019-01-05 16:15:40 -0600

    testing sample code from exercise

--- easy5.rb
+++ easy5.rb
@@ -108,19 +108,33 @@
 
 ####. LETTER SWAP
 
-def swap(string)
+# def swap(string)
 
-  swapped = string.split.map do |word|
-    first = word[0]
-    word[0] = word[-1]
-    word[-1] = first
-    word
-  end 
+#   swapped = string.split.map do |word|
+#     first = word[0]
+#     word[0] = word[-1]
+#     word[-1] = first
+#     word
+#   end 
 
-swapped.join(' ')
+# swapped.join(' ')
 
 
- end  
+#  end  
+
+
+
+
+# def swap_first_last_characters(a, b)
+#   a, b = b, a
+# end
+
+# def swap(words)
+#   result = words.split.map do |word|
+#     p swap_first_last_characters(word[0], word[-1])
+#   end
+#   result.join(' ')
+# end
 
  p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
 p swap('Abcde') == 'ebcdA'
