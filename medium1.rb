e492697b1e4981565f2d7d928a830b2dc27d157c
mandlerm <amaya.melissa@gmail.com>
2019-01-15 15:39:25 -0600

    print, push and mult working

--- medium1.rb
+++ medium1.rb
@@ -102,31 +102,117 @@ require 'pry'
 # => create a string *.current_num times
 # => print string to screen, centered with n being argument to center
 
-def diamond(size)
-  1.step(by: 2, to: size) { |num| print_row(num, size) }
-  (size-2).step(by: -2, to: 1) { |num| print_row(num, size) }
-end
+# def diamond(size)
+#   1.step(by: 2, to: size) { |num| print_row(num, size) }
+#   (size-2).step(by: -2, to: 1) { |num| print_row(num, size) }
+# end
+
+# # def print_row(num_of_stars, size)
+# #   string = ''
+# #   num_of_stars.times { string << "*" }
+# #   puts string.center(size)  
+# # end
 
 # def print_row(num_of_stars, size)
 #   string = ''
-#   num_of_stars.times { string << "*" }
+#   num_of_stars.times { string << " " }
+#   string[0] = "*"
+#   string[-1] = "*"
 #   puts string.center(size)  
 # end
 
-def print_row(num_of_stars, size)
-  string = ''
-  num_of_stars.times { string << " " }
-  string[0] = "*"
-  string[-1] = "*"
-  puts string.center(size)  
-end
-
-# diamond(1)
-# diamond(3)
-diamond(9)
+# # diamond(1)
+# # diamond(3)
+# diamond(9)
 
 ##### 6
 
+# INPUT: string of instructions - parameter str
+# OUTPUT:
+# NEEDS: 
+# => instructions = str.split
+# => interact with the shift value of array one at a time
+# => IF val == integer, store this value in the register
+# => 
+# => write helper mthod for each instruction
+
+def print(register)
+  p register
+end
+
+def r_push(register, stack)
+  stack.unshift(register)
+end
+
+# need to capture return value
+def r_add(register, stack)
+  register += stack.shift
+end
+
+# def r_print(register)
+#   p register
+# end
+
+def r_mult(register, stack)
+  register * stack.shift
+end
+
+def minilang(instructions)
+  register = 0
+  stack = []
+  instructions.split.each do |command|
+    if command.to_i.to_s == command
+      register = command.to_i
+    else
+     case command
+     when "PRINT"
+      print(register)
+     when "PUSH"
+      r_push(register, stack)
+      # register is replaced by new number
+     when "MULT"
+      register = r_mult(register, stack)
+     end
+
+    end 
+  end
+end
+
+
+# minilang("3 PUSH PRINT 5 PUSH PRINT")
+# minilang("3 PUSH 4 ADD PRINT")
+
+# minilang('PRINT')
+# # 0
+
+minilang('5 PUSH 3 MULT PRINT')
+# # 15
+
+# minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
+# # 5
+# # 3
+# # 8
+
+# minilang('5 PUSH POP PRINT')
+# # 5
+
+# minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+# # 5
+# # 10
+# # 4
+# # 7
+
+# minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
+# # 6
+
+# minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
+# # 12
+
+# minilang('-3 PUSH 5 SUB PRINT')
+# # 8
+
+# minilang('6 PUSH')
+# (nothing printed; no PRINT commands)
 
 
 ##### 7
