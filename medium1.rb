a2f784dcf49e77696350c83c90cbafe8da552f11
mandlerm <amaya.melissa@gmail.com>
2019-01-15 21:38:41 -0600

    finished medium 1 problems

--- medium1.rb
+++ medium1.rb
@@ -136,48 +136,70 @@ require 'pry'
 # => 
 # => write helper mthod for each instruction
 
-def print(register)
-  p register
-end
-
-def r_push(register, stack)
-  stack.unshift(register)
-end
-
-# need to capture return value
-def r_add(register, stack)
-  register += stack.shift
-end
-
 # def r_print(register)
 #   p register
 # end
 
-def r_mult(register, stack)
-  register * stack.shift
-end
+# def r_push(register, stack)
+#   stack.unshift(register)
+# end
 
-def minilang(instructions)
-  register = 0
-  stack = []
-  instructions.split.each do |command|
-    if command.to_i.to_s == command
-      register = command.to_i
-    else
-     case command
-     when "PRINT"
-      print(register)
-     when "PUSH"
-      r_push(register, stack)
-      # register is replaced by new number
-     when "MULT"
-      register = r_mult(register, stack)
-     end
+# # need to capture return value
+# def r_add(register, stack)
+#   register += stack.shift
+# end
 
-    end 
-  end
-end
+# def r_sub(register, stack)
+#   register -= stack.shift
+# end
 
+# def r_pop(stack)
+#   stack.shift
+# end
+
+# def r_mult(register, stack)
+#   register * stack.shift
+# end
+
+# def r_div(register, stack)
+#   register / stack.shift
+# end
+
+# def r_mod(register, stack)
+#   register % stack.shift
+# end
+
+# def minilang(instructions)
+#   register = 0
+#   stack = []
+#   instructions.split.each do |command|
+#     if command.to_i.to_s == command
+#       register = command.to_i
+#     else
+#      case command
+#      when "PRINT"
+#       r_print(register)
+#      when "PUSH"
+#       r_push(register, stack)
+#      when "MULT"
+#       register = r_mult(register, stack)
+#      when "ADD"
+#      register = r_add(register, stack)
+#      when "POP"
+#      register = r_pop(stack) 
+#      when "DIV"
+#       register = r_div(register, stack)
+#     when "MOD"
+#       register = r_mod(register, stack)
+#     when "SUB"
+#       register = r_sub(register, stack)
+#     end
+#     end 
+#   end
+# end
+
+
+# def 
 
 # minilang("3 PUSH PRINT 5 PUSH PRINT")
 # minilang("3 PUSH 4 ADD PRINT")
@@ -185,7 +207,7 @@ end
 # minilang('PRINT')
 # # 0
 
-minilang('5 PUSH 3 MULT PRINT')
+# minilang('5 PUSH 3 MULT PRINT')
 # # 15
 
 # minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
@@ -209,7 +231,7 @@ minilang('5 PUSH 3 MULT PRINT')
 # # 12
 
 # minilang('-3 PUSH 5 SUB PRINT')
-# # 8
+# # # 8
 
 # minilang('6 PUSH')
 # (nothing printed; no PRINT commands)
@@ -217,13 +239,88 @@ minilang('5 PUSH 3 MULT PRINT')
 
 ##### 7
 
+# INPUT: string containing words and numbers (as words)
+# OUTPUT: change the numbers into integers. Return the SAME string.
+
+# DO: cannot split string! must be same string.
+# => great hash for number word => integer
+# => iterate through the hash
+# => gsub each one
+# => if hsh[key] == item, item.gsub(hsh[key])
+# =>  return the string
+
+# def word_to_digit(string)
+#   number_words = {"one" => 1, "two" => 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, "nine" => 9, "zero" => 0}
+#   number_words.each do |k, val| 
+#     string.gsub!(/\b#{k}\b/, val.to_s)
+#   end
+#     string
+# end
+
+# p word_to_digit('Please call me at five five five one two three four. Thanks. Freight, niner, twofer') #== 'Please call me at 5 5 5 1 2 3 4. Thanks.'
+
 
 ##### 8
 
+# INPUT: integer
+# OUTPUT: fibonacci number for that integer
+# DO: 1 + 1 = 2
+# =>      1 + 2 = 3
+# =>          2 + 3 = 5
+# =>              3 + 5 = 8
 
+# def fibonacci(num)
+#   return 1 if num <= 2
+#   fibonacci(num - 1) + fibonacci(num - 2)
+# end
+
+
+# p fibonacci(1) == 1
+# p fibonacci(2) == 1
+# p fibonacci(3) == 2
+# p fibonacci(4) == 3
+# p fibonacci(5) == 5
+# p fibonacci(12) == 144
+# p fibonacci(20) == 6765
 
 ##### 9
 
+# def fibonacci(num)
+#   fib = 0
+#   if num <= 2
+#     return 1
+#   else 
+#     num1 = 1
+#     num2 = 1
+#     fib = 0
+#     (num-2).times do |n|
+#       fib = num1 + num2
+#       num1 = num2
+#       num2 = fib 
+#     end
+#   end
+#   p fib
+# end
 
+# # p fibonacci(20) == 6765
+# # p fibonacci(100) == 354224848179261915075
+# p fibonacci(100_001) # => 4202692702.....8285979669707537501
 
 ##### 10
+
+def fibonacci_last(num)
+  first, last = [1, 1]
+
+  3.upto(num) do 
+    first, last = [last, first + last]
+  end
+  last.to_s[-1].to_i
+end
+
+
+p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+# p fibonacci_last(123456789) # -> 4
